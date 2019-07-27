a7dc36d2cf02cc1ef09fb8909573854743326fda
Terje <terjekir@student.matnat.uio.no>
2019-07-27 12:33:15 +0200

    Small formatting changes

--- src/defs.h
+++ src/defs.h
@@ -35,11 +35,12 @@ typedef unsigned long long U64;
 #define INFINITE 30000
 #define ISMATE (INFINITE - MAXDEPTH)
 
-enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
+enum { WHITE, BLACK, BOTH };
 enum { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
+enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
 enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
 enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
-enum { WHITE, BLACK, BOTH };
+
 enum { UCIMODE, XBOARDMODE, CONSOLEMODE };
 
 enum {
