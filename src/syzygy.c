b902880ecde5ae14c3b146bf65c0636fa184897f
Terje <terjekir@student.matnat.uio.no>
2019-07-28 02:37:52 +0200

    Fix mistake in countPieces

--- src/syzygy.c
+++ src/syzygy.c
@@ -3,7 +3,7 @@
 #include "defs.h"
 #include "fathom/tbprobe.h"
 
-#define TB_PROBE_DEPTH 0
+#define TB_PROBE_DEPTH 2
 #define TB_LARGEST 6
 
 int countPieces(S_BOARD *pos) {
@@ -11,8 +11,7 @@ int countPieces(S_BOARD *pos) {
     int cardinality = 0;
 
     for (int pce = wP; pce <= bK; ++pce) {
-		if (pos->pceNum[pce] < 0 || pos->pceNum[pce] >= 10)
-			cardinality += pos->pceNum[pce];
+		cardinality += pos->pceNum[pce];
 	}
 
     return cardinality;
@@ -22,12 +21,14 @@ unsigned int probeWDL(S_BOARD *pos, int depth) {
     
     int cardinality = countPieces(pos);
 
-    if (    pos->ply == 0
-        ||  pos->enPas != NO_SQ
-        ||  pos->castlePerm != 0
-        ||  pos->fiftyMove != 0
-        ||  cardinality > (int)TB_LARGEST
-        || (cardinality == (int)TB_LARGEST && depth < (int)TB_PROBE_DEPTH))
+    ASSERT(CheckBoard(pos));
+
+    if (    (pos->ply == 0)
+        ||  (pos->enPas != NO_SQ)
+        ||  (pos->castlePerm != 0)
+        ||  (pos->fiftyMove != 0)
+        ||  (cardinality > (int)TB_LARGEST)
+        ||  (cardinality == (int)TB_LARGEST && depth < (int)TB_PROBE_DEPTH))
         return TB_RESULT_FAILED;
 
     return tb_probe_wdl(
