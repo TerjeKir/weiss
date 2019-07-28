02ba4ea93cbdb21535c280881af7b22ecb2e9a19
Terje <terjekir@student.matnat.uio.no>
2019-07-28 03:15:24 +0200

    Add fast popcounts

--- src/syzygy.c
+++ src/syzygy.c
@@ -3,18 +3,37 @@
 #include "defs.h"
 #include "fathom/tbprobe.h"
 
-#define TB_PROBE_DEPTH 2
+#define TB_PROBE_DEPTH 4
 #define TB_LARGEST 6
+
+const uint64_t m1  = 0x5555555555555555;
+const uint64_t m2  = 0x3333333333333333;
+const uint64_t m4  = 0x0f0f0f0f0f0f0f0f;
+const uint64_t h01 = 0x0101010101010101;
 
 int countPieces(S_BOARD *pos) {
 
-    int cardinality = 0;
+    long long unsigned int x = pos->colors[WHITE] | pos->colors[BLACK];
 
-    for (int pce = wP; pce <= bK; ++pce) {
-		cardinality += pos->pceNum[pce];
-	}
+    //// Best for slow multiplication
+    x -= (x >> 1) & m1;              //put count of each 2 bits into those 2 bits
+    x = (x & m2) + ((x >> 2) & m2);  //put count of each 4 bits into those 4 bits 
+    x = (x + (x >> 4)) & m4;         //put count of each 8 bits into those 8 bits 
+    x += x >>  8;                    //put count of each 16 bits into their lowest 8 bits
+    x += x >> 16;                    //put count of each 32 bits into their lowest 8 bits
+    x += x >> 32;                    //put count of each 64 bits into their lowest 8 bits
+    
+    return x & 0x7f;
 
-    return cardinality;
+    //// Best for fast multiplication
+    // x -= (x >> 1) & m1;                 //put count of each 2 bits into those 2 bits
+    // x = (x & m2) + ((x >> 2) & m2);     //put count of each 4 bits into those 4 bits 
+    // x = (x + (x >> 4)) & m4;            //put count of each 8 bits into those 8 bits 
+
+    // return (x * h01) >> 56;             //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ... 
+
+    //// Built-in
+    // return __builtin_popcount(x);
 }
 
 unsigned int probeWDL(S_BOARD *pos, int depth) {
