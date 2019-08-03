0241dc8f8433238fd10e52816c0cd049ea92ab4f
Terje <terjekir@student.matnat.uio.no>
2019-08-03 19:19:19 +0200

    Small cleanup in attack.c

--- src/attack.c
+++ src/attack.c
@@ -5,13 +5,11 @@
 #include "defs.h"
 #include "attack.h"
 #include "bitboards.h"
-#include "validate.h"
 #include "board.h"
+#include "validate.h"
 
-const int KnDir[8] = {-8, -19, -21, -12, 8, 19, 21, 12};
 const int RkDir[4] = {-1, -10, 1, 10};
 const int BiDir[4] = {-9, -11, 11, 9};
-const int KiDir[8] = {-1, -10, 1, 10, -9, -11, 11, 9};
 
 uint64_t king_attacks[64];
 uint64_t knight_attacks[64];
