fbb4c9abaf40eba343a3e2f4bb227c994048041c
Terje <terjekir@student.matnat.uio.no>
2019-08-07 12:16:53 +0200

    Remove material draw from eval, TBs will do it

--- src/evaluate.c
+++ src/evaluate.c
@@ -77,39 +77,6 @@ const int KingE[64] = {
 	-50, -10, 0,   0,  0, 0,  -10, -50};
 
 
-static int MaterialDraw(const S_BOARD *pos) {
-
-	assert(CheckBoard(pos));
-
-	if (!pos->pceNum[wR] && !pos->pceNum[bR] && !pos->pceNum[wQ] && !pos->pceNum[bQ]) {
-		if (!pos->pceNum[bB] && !pos->pceNum[wB]) {
-			if (pos->pceNum[wN] < 3 && pos->pceNum[bN] < 3)
-				return TRUE;
-		
-		} else if (!pos->pceNum[wN] && !pos->pceNum[bN]) {
-			if (abs(pos->pceNum[wB] - pos->pceNum[bB]) < 2)
-				return TRUE;
-
-		} else if ((pos->pceNum[wN] < 3 && !pos->pceNum[wB]) || (pos->pceNum[wB] == 1 && !pos->pceNum[wN]))
-			if ((pos->pceNum[bN] < 3 && !pos->pceNum[bB]) || (pos->pceNum[bB] == 1 && !pos->pceNum[bN]))
-				return TRUE;
-
-	} else if (!pos->pceNum[wQ] && !pos->pceNum[bQ]) {
-		if (pos->pceNum[wR] == 1 && pos->pceNum[bR] == 1) {
-			if ((pos->pceNum[wN] + pos->pceNum[wB]) < 2 && (pos->pceNum[bN] + pos->pceNum[bB]) < 2)
-				return TRUE;
-
-		} else if (pos->pceNum[wR] == 1 && !pos->pceNum[bR]) {
-			if ((pos->pceNum[wN] + pos->pceNum[wB] == 0) && (((pos->pceNum[bN] + pos->pceNum[bB]) == 1) || ((pos->pceNum[bN] + pos->pceNum[bB]) == 2)))
-				return TRUE;
-
-		} else if (pos->pceNum[bR] == 1 && !pos->pceNum[wR])
-			if ((pos->pceNum[bN] + pos->pceNum[bB] == 0) && (((pos->pceNum[wN] + pos->pceNum[wB]) == 1) || ((pos->pceNum[wN] + pos->pceNum[wB]) == 2)))
-				return TRUE;
-	}
-	return FALSE;
-}
-
 int EvalPosition(const S_BOARD *pos) {
 
 	assert(CheckBoard(pos));
@@ -118,9 +85,6 @@ int EvalPosition(const S_BOARD *pos) {
 	int pceNum;
 	int sq;
 	int score = pos->material[WHITE] - pos->material[BLACK];
-
-	if (!pos->pceNum[wP] && !pos->pceNum[bP] && MaterialDraw(pos))
-		return 0;
 
 	pce = wP;
 	for (pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
