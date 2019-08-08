f0ccbdc4d3cdd50d231cf70037bafee5a0c730f8
Terje <terjekir@student.matnat.uio.no>
2019-08-08 13:25:21 +0200

    Add attack bbs to header for use in movegen

--- src/attack.h
+++ src/attack.h
@@ -4,6 +4,10 @@
 
 #include "defs.h"
 
+uint64_t king_attacks[64];
+uint64_t knight_attacks[64];
+uint64_t pawn_attacks[2][64];
+
 void InitAttacks();
 
 int SqAttacked(const int sq, const int side, const S_BOARD *pos);
