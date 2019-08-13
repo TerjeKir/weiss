c4093bc0b8ca8e9cf192dfcf72db0140aaa5cd38
Terje <terjekir@student.matnat.uio.no>
2019-08-13 23:32:39 +0200

    Add magic bitboards

--- src/attack.c
+++ src/attack.c
@@ -8,12 +8,29 @@
 #include "board.h"
 #include "validate.h"
 
-const int RkDir[4] = {-1, -10, 1, 10};
-const int BiDir[4] = {-9, -11, 11, 9};
 
-bitboard king_attacks[64];
-bitboard knight_attacks[64];
+const int bishopDirections[4] = {7, 9, -7, -9};
+const int   rookDirections[4] = {8, 1, -8, -1};
+
+typedef struct {
+
+    bitboard *attacks;
+    bitboard mask;
+    uint64_t magic;
+    int shift;
+
+} MAGIC;
+
+// Simple
 bitboard pawn_attacks[2][64];
+bitboard knight_attacks[64];
+bitboard king_attacks[64];
+// Magic
+bitboard bishop_attacks[0x1480];
+bitboard rook_attacks[0x19000];
+
+MAGIC mBishopTable[64];
+MAGIC mRookTable[64];
 
 
 // Inits the king attack bitboards
@@ -104,78 +121,109 @@ static void InitPawnAttacks() {
     }
 }
 
+static bitboard SliderAttacks(int sq, bitboard occupied, MAGIC *table) {
+    bitboard *ptr = table[sq].attacks;
+    occupied     &= table[sq].mask;
+    occupied     *= table[sq].magic;
+    occupied    >>= table[sq].shift;
+    return ptr[occupied];
+}
+
+static bitboard MakeSliderAttacks(int sq, bitboard occupied, const int directions[]) {
+
+    bitboard result = 0;
+
+    for (int dir = 0; dir < 4; dir++)
+
+        for (int tSq = sq + directions[dir];
+             (tSq >= 0) && (tSq < 64) && (Distance(tSq, tSq - directions[dir]) == 1);
+             tSq += directions[dir]) {
+
+            SETBIT(result, tSq);
+
+            if (occupied & (1ULL << tSq))
+                break;
+        }
+
+    return result;
+}
+
+// Inits the magic shit
+static void InitSliderAttacks(MAGIC *table, bitboard *attackTable, const bitboard *magics, const int *dir) {
+
+    int size, index;
+    bitboard edges, blockers;
+
+    table[0].attacks = attackTable;
+
+    for (int sq = 0; sq < 64; sq++) {
+
+        edges = ((rank1BB | rank8BB) & ~rankBBs[sq / 8]) 
+              | ((fileABB | fileHBB) & ~fileBBs[sq % 8]);
+
+        table[sq].magic = magics[sq];
+        table[sq].mask  = MakeSliderAttacks(sq, 0, dir) & ~edges;
+        table[sq].shift = 64 - PopCount(table[sq].mask);
+
+        table[sq].attacks = sq == 0 ? attackTable : table[sq - 1].attacks + size + 1;
+
+        size = blockers = 0;
+
+        do {
+            index = (blockers * table[sq].magic) >> table[sq].shift;
+            if (index > size) 
+                size = index;
+            table[sq].attacks[index] = MakeSliderAttacks(sq, blockers, dir);
+            blockers = (blockers - table[sq].mask) & table[sq].mask; // Carry rippler
+        } while (blockers);
+    }
+}
+
 // Initializes all attack bitboards
 void InitAttacks() {
     InitKingAttacks();
     InitKnightAttacks();
     InitPawnAttacks();
+    InitSliderAttacks(mBishopTable, bishop_attacks, BishopMagics, bishopDirections);
+    InitSliderAttacks(mRookTable,     rook_attacks,   RookMagics,   rookDirections);
 }
 
 // Returns TRUE if sq is attacked by side
 int SqAttacked(const int sq, const int side, const S_BOARD *pos) {
 	
-	int pce, index, t_sq, dir;
+    bitboard bAtks, rAtks;
 	int sq64 = SQ64(sq);
 
 	assert(SqOnBoard(sq));
+    assert(sq64 >= 0);
+    assert(sq64 < 64);
 	assert(SideValid(side));
 	assert(CheckBoard(pos));
 
-	// pawns
+    bitboard allPieces = pos->colors[WHITE] | pos->colors[BLACK];
+    bitboard bishops = pos->colors[side] & (pos->pieceBBs[BISHOP] | pos->pieceBBs[QUEEN]);
+    bitboard rooks = pos->colors[side] & (pos->pieceBBs[ROOK] | pos->pieceBBs[QUEEN]);
+
+	// Pawns
 	if (pawn_attacks[!side][sq64] & pos->pieceBBs[PAWN] & pos->colors[side])
 		return TRUE;
 
-	// knights
+	// Knights
 	if (knight_attacks[sq64] & pos->pieceBBs[KNIGHT] & pos->colors[side])
 		return TRUE;
 
-    // kings
+    // Kings
 	if (king_attacks[sq64] & pos->pieceBBs[KING] & pos->colors[side])
 		return TRUE;
 
-	// rooks, queens
-	for (index = 0; index < 4; ++index) {
+    // Bishops
+    bAtks = SliderAttacks(sq64, allPieces, mBishopTable);
+    if (bAtks & bishops) return TRUE;
 
-		dir = RkDir[index];
-		t_sq = sq + dir;
-		assert(SqIs120(t_sq));
 
-		pce = pos->pieces[t_sq];
-		assert(PceValidEmptyOffbrd(pce));
-
-		while (pce != OFFBOARD) {
-			if (pce != EMPTY) {
-				if (IsRQ(pce) && PieceCol[pce] == side)
-					return TRUE;
-				break;
-			}
-			t_sq += dir;
-			assert(SqIs120(t_sq));
-			pce = pos->pieces[t_sq];
-		}
-	}
-
-	// bishops, queens
-	for (index = 0; index < 4; ++index) {
-
-		dir = BiDir[index];
-		t_sq = sq + dir;
-		assert(SqIs120(t_sq));
-
-		pce = pos->pieces[t_sq];
-		assert(PceValidEmptyOffbrd(pce));
-
-		while (pce != OFFBOARD) {
-			if (pce != EMPTY) {
-				if (IsBQ(pce) && PieceCol[pce] == side)
-					return TRUE;
-				break;
-			}
-			t_sq += dir;
-			assert(SqIs120(t_sq));
-			pce = pos->pieces[t_sq];
-		}
-	}
+    // Rook
+    rAtks = SliderAttacks(sq64, allPieces, mRookTable);
+    if (rAtks & rooks) return TRUE;
 
 	return FALSE;
 }
