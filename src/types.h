793bae2a86550569f17e698a4a2cff44412c7aa6
Terje <terjekir@student.matnat.uio.no>
2019-10-01 03:52:08 +0200

    types.h enums

--- src/types.h
+++ src/types.h
@@ -10,26 +10,27 @@
 
 typedef uint64_t bitboard;
 
+enum Protocol { UCIMODE, CONSOLEMODE };
+
+enum Limit { MAXGAMEMOVES     = 512,
+	         MAXPOSITIONMOVES = 256,
+	         MAXDEPTH         = 128 };
+
+enum Score { INFINITE = 30000,
+			 ISMATE   = INFINITE - MAXDEPTH };
+
 typedef enum Color { BLACK, WHITE, BOTH } Color;
 
-enum { MAXGAMEMOVES 	= 512,
-	   MAXPOSITIONMOVES = 256,
-	   MAXDEPTH 		= 128 };
-
-enum { INFINITE = 30000,
-	   ISMATE 	= INFINITE - MAXDEPTH };
-
+// TODO: Have to fix fathom enums before naming these
 enum { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
 
 enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
 
-enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
+enum File { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
 
-enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
+enum Rank { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
 
-enum { UCIMODE, CONSOLEMODE };
-
-enum {
+enum Square {
   A1, B1, C1, D1, E1, F1, G1, H1,
   A2, B2, C2, D2, E2, F2, G2, H2,
   A3, B3, C3, D3, E3, F3, G3, H3,
@@ -40,7 +41,7 @@ enum {
   A8, B8, C8, D8, E8, F8, G8, H8, NO_SQ = 99
 };
 
-enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
+enum CastlingRights { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
 
 /* STRUCTS */
 
