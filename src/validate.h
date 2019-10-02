43767a8a035a507e3c970832d253798891fe433d
Terje <terjekir@student.matnat.uio.no>
2019-10-02 13:41:43 +0200

    validate.h param names fix

--- src/validate.h
+++ src/validate.h
@@ -8,7 +8,7 @@
 #ifndef NDEBUG
 int ValidSquare(const int sq);
 int ValidSide(const int side);
-int ValidPiece(const int pce);
-int ValidPieceOrEmpty(const int pce);
+int ValidPiece(const int piece);
+int ValidPieceOrEmpty(const int piece);
 int MoveListOk(const S_MOVELIST *list,  const S_BOARD *pos);
 #endif
