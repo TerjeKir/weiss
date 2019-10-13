e19069aec0d06812d4107d9b242677d8fc879bd7
Terje <terjekir@student.matnat.uio.no>
2019-10-13 17:53:59 +0200

    Benchmark added in CLI mode

--- src/tests.h
+++ src/tests.h
@@ -7,6 +7,7 @@
 #include "types.h"
 
 
+void benchmark(int depth, S_BOARD *pos, S_SEARCHINFO *info);
 void Perft(const int depth, S_BOARD *pos);
 void MirrorEvalTest(S_BOARD *pos);
 void MateInXTest(S_BOARD *pos);
