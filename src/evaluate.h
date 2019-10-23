d8985c05fda75993d624722cc82e9beff77bcbea
Terje <terjekir@student.matnat.uio.no>
2019-10-24 01:24:37 +0200

    Add eg/mg helper "functions"

--- src/evaluate.h
+++ src/evaluate.h
@@ -5,4 +5,10 @@
 #include "types.h"
 
 
+#define MakeScore(mg, eg) ((int)((unsigned int)(eg) << 16) + (mg))
+#define S(mg, eg) MakeScore((mg), (eg))
+#define MgScore(s) ((int16_t)((uint16_t)((unsigned)((s)))))
+#define EgScore(s) ((int16_t)((uint16_t)((unsigned)((s) + 0x8000) >> 16)))
+
+
 int EvalPosition(const Position *pos);
