e5557d021c9584758226688e03a747573d897341
Terje <terjekir@student.matnat.uio.no>
2020-01-02 14:36:12 +0100

    switch to monotonic clock

--- src/time.c
+++ src/time.c
@@ -3,7 +3,7 @@
 #ifdef _WIN32
 #include <windows.h>
 #else
-#include <sys/time.h>
+#include <time.h>
 #endif
 
 
@@ -12,7 +12,7 @@ int now() {
     return GetTickCount();
 #else
     struct timeval t;
-    gettimeofday(&t, NULL);
+    clock_gettime(CLOCK_MONOTONIC, t)
     return t.tv_sec * 1000 + t.tv_usec / 1000;
 #endif
 }
