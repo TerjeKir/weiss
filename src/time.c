f4f4004ad1210a98f9df5c43002973ae152ec06f
Terje <terjekir@student.matnat.uio.no>
2020-01-02 14:56:57 +0100

    ;

--- src/time.c
+++ src/time.c
@@ -12,7 +12,7 @@ int now() {
     return GetTickCount();
 #else
     struct timespec t;
-    clock_gettime(CLOCK_MONOTONIC, &t)
+    clock_gettime(CLOCK_MONOTONIC, &t);
     return t.tv_sec * 1000 + t.tv_nsec / 1000000;
 #endif
 }
