f7c531a7b34cfe51fb42644a4d6354b7ddb97243
TerjeKir <42723273+TerjeKir@users.noreply.github.com>
2020-01-19 02:23:57 +0100

    Speedup MakeMove (#141)
    Only rehash the castling rights if at least one side can castle, and either the to or from square is the original square of a king or rook.
    
    ELO | 5.59 +- 4.30 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=32MB
    LLR | 3.06 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 15527 W: 4951 L: 4701 D: 5875
    http://chess.grantnet.us/viewTest/4361/

--- src/makemove.c
+++ src/makemove.c
@@ -242,11 +242,15 @@ bool MakeMove(Position *pos, const int move) {
         pos->enPas = NO_SQ;
     }
 
-    // Hash out the old castling rights, update and hash back in
-    HASH_CA;
-    pos->castlePerm &= CastlePerm[from];
-    pos->castlePerm &= CastlePerm[to];
-    HASH_CA;
+    // Rehash the castling rights if at least one side can castle,
+    // and either the to or from square is the original square of
+    // a king or rook.
+    if (pos->castlePerm && CastlePerm[from] ^ CastlePerm[to]) {
+        HASH_CA;
+        pos->castlePerm &= CastlePerm[from];
+        pos->castlePerm &= CastlePerm[to];
+        HASH_CA;
+    }
 
     // Move the rook during castling
     if (move & FLAG_CASTLE)
