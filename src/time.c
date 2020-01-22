421b97bd30706058239ce17756c55f9ea8d43aa7
Terje <terjekir@student.matnat.uio.no>
2020-01-22 19:12:05 +0100

    use clock_gettime on windows as well (finer grain)

--- src/time.c
+++ src/time.c
@@ -1,20 +1,12 @@
 // time.c
 
-#ifdef _WIN32
-#include <windows.h>
-#else
 #include <time.h>
-#endif
 
 
 int Now() {
-#if defined(_WIN32) || defined(_WIN64)
-    return GetTickCount();
-#else
     struct timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
     return t.tv_sec * 1000 + t.tv_nsec / 1000000;
-#endif
 }
 
 int TimeSince(const int time) {
