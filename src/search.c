e00676ddf931ca78d47f01b3d8ef208653dd2e0d
Terje <terjekir@student.matnat.uio.no>
2020-02-05 02:03:10 +0100

    improving

--- src/search.c
+++ src/search.c
@@ -296,6 +296,7 @@ static int AlphaBeta(int alpha, int beta, int depth, Position *pos, SearchInfo *
 
     int score = -INFINITE;
     int eval = NOSCORE;
+    bool improving = false;
 
     // Skip pruning while in check and at the root
     if (!inCheck && !root) {
@@ -305,6 +306,9 @@ static int AlphaBeta(int alpha, int beta, int depth, Position *pos, SearchInfo *
             history(0).eval = eval = -history(-1).eval;
         else
             history(0).eval = eval = EvalPosition(pos);
+
+        if (pos->ply >= 2 && eval > history(-2).eval)
+            improving = true;
 
         // Razoring
         if (!pvNode && depth < 2 && eval + 640 < alpha)
@@ -360,7 +364,7 @@ static int AlphaBeta(int alpha, int beta, int depth, Position *pos, SearchInfo *
         bool quiet = !moveIsNoisy(move);
 
         // Late move pruning
-        if (!pvNode && !inCheck && quiet && depth <= 3 && quietCount > 2.5 * depth * depth)
+        if (!pvNode && !inCheck && quiet && depth <= 3 && quietCount > 4 * depth * depth / (1 + !improving))
             break;
 
         // Make the move, skipping to the next if illegal
