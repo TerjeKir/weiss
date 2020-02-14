75cdd120c578f557960f11ba4b9e65c3ac339286
TerjeKir <42723273+TerjeKir@users.noreply.github.com>
2020-02-14 21:07:17 +0100

    Weiss 0.9 (#179)
    Since 0.8 a little over a month ago Weiss has gained about 40 self-play elo:
    
    ELO | 43.76 +- 3.22 (95%)
    Games | W: 10488 L: 6974 D: 10582

--- src/uci.c
+++ src/uci.c
@@ -31,7 +31,7 @@
 #include "transposition.h"
 
 
-#define NAME "weiss 0.8-dev"
+#define NAME "weiss 0.9"
 
 #define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 #define INPUT_SIZE 4096
