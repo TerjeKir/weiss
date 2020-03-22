36a13e689dfcba5875858a3a4dfe5a00eb323f7f
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-03-22 04:04:21 +0100

    cleanup CheckBoard

--- src/board.c
+++ src/board.c
@@ -303,46 +303,43 @@ bool CheckBoard(const Position *pos) {
     assert(0 <= pos->gamePly && pos->gamePly < MAXGAMEMOVES);
     assert(    0 <= pos->ply && pos->ply < MAXDEPTH);
 
-    int t_pieceCounts[PIECE_NB] = { 0 };
-    int t_bigPieces[2] = { 0, 0 };
+    int counts[PIECE_NB] = { 0 };
+    int nonPawnCount[2] = { 0, 0 };
 
-    int t_piece, color;
+    for (Square sq = A1; sq <= H8; ++sq) {
 
-    // Bitboards
-    assert(PopCount(pieceBB(PAWN)   & colorBB(WHITE)) <= 8);
-    assert(PopCount(pieceBB(KNIGHT) & colorBB(WHITE)) <= 10);
-    assert(PopCount(pieceBB(BISHOP) & colorBB(WHITE)) <= 10);
-    assert(PopCount(pieceBB(ROOK)   & colorBB(WHITE)) <= 10);
-    assert(PopCount(pieceBB(QUEEN)  & colorBB(WHITE)) <= 9);
-    assert(PopCount(pieceBB(KING)   & colorBB(WHITE)) == 1);
+        Piece piece = pieceOn(sq);
+        counts[piece]++;
+        Color color = ColorOf(piece);
+        nonPawnCount[color] += NonPawn[piece];
+    }
 
-    assert(PopCount(pieceBB(PAWN)   & colorBB(BLACK)) <= 8);
-    assert(PopCount(pieceBB(KNIGHT) & colorBB(BLACK)) <= 10);
-    assert(PopCount(pieceBB(BISHOP) & colorBB(BLACK)) <= 10);
-    assert(PopCount(pieceBB(ROOK)   & colorBB(BLACK)) <= 10);
-    assert(PopCount(pieceBB(QUEEN)  & colorBB(BLACK)) <= 9);
-    assert(PopCount(pieceBB(KING)   & colorBB(BLACK)) == 1);
+    assert(PopCount(colorPieceBB(WHITE, PAWN))   == counts[MakePiece(WHITE, PAWN)]);
+    assert(PopCount(colorPieceBB(WHITE, KNIGHT)) == counts[MakePiece(WHITE, KNIGHT)]);
+    assert(PopCount(colorPieceBB(WHITE, BISHOP)) == counts[MakePiece(WHITE, BISHOP)]);
+    assert(PopCount(colorPieceBB(WHITE, ROOK))   == counts[MakePiece(WHITE, ROOK)]);
+    assert(PopCount(colorPieceBB(WHITE, QUEEN))  == counts[MakePiece(WHITE, QUEEN)]);
+    assert(PopCount(colorPieceBB(WHITE, KING))   == counts[MakePiece(WHITE, KING)]);
+
+    assert(PopCount(colorPieceBB(BLACK, PAWN))   == counts[MakePiece(BLACK, PAWN)]);
+    assert(PopCount(colorPieceBB(BLACK, KNIGHT)) == counts[MakePiece(BLACK, KNIGHT)]);
+    assert(PopCount(colorPieceBB(BLACK, BISHOP)) == counts[MakePiece(BLACK, BISHOP)]);
+    assert(PopCount(colorPieceBB(BLACK, ROOK))   == counts[MakePiece(BLACK, ROOK)]);
+    assert(PopCount(colorPieceBB(BLACK, QUEEN))  == counts[MakePiece(BLACK, QUEEN)]);
+    assert(PopCount(colorPieceBB(BLACK, KING))   == counts[MakePiece(BLACK, KING)]);
 
     assert(pieceBB(ALL) == (colorBB(WHITE) | colorBB(BLACK)));
 
-    // check piece count and other counters
-    for (Square sq = A1; sq <= H8; ++sq) {
-
-        t_piece = pieceOn(sq);
-        t_pieceCounts[t_piece]++;
-        color = ColorOf(t_piece);
-
-        if (NonPawn[t_piece]) t_bigPieces[color]++;
-    }
-
-    assert(t_bigPieces[WHITE] == pos->nonPawnCount[WHITE] && t_bigPieces[BLACK] == pos->nonPawnCount[BLACK]);
+    assert(nonPawnCount[WHITE] == pos->nonPawnCount[WHITE]
+        && nonPawnCount[BLACK] == pos->nonPawnCount[BLACK]);
 
     assert(sideToMove == WHITE || sideToMove == BLACK);
 
     assert(pos->epSquare == NO_SQ
        || (RelativeRank(sideToMove, RankOf(pos->epSquare)) == RANK_6));
 
-    assert(pos->castlingRights >= 0 && pos->castlingRights <= 15);
+    assert(pos->castlingRights >= 0
+        && pos->castlingRights <= 15);
 
     assert(GeneratePosKey(pos) == pos->key);
 
