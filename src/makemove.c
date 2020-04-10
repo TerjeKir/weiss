383e68b230bb9d59f594b17e971cc29c464184ce
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-04-10 21:29:55 +0200

    cleanup makemove

--- src/makemove.c
+++ src/makemove.c
@@ -192,7 +192,6 @@ bool MakeMove(Position *pos, const Move move) {
     const Square from = fromSq(move);
     const Square to   = toSq(move);
     const Piece capt  = capturing(move);
-    const Color color = sideToMove;
 
     // Save position
     history(0).posKey         = pos->key;
@@ -236,8 +235,6 @@ bool MakeMove(Position *pos, const Move move) {
     else if (capt != EMPTY) {
         assert(ValidPiece(capt));
         ClearPiece(pos, to);
-
-        // Reset 50mr after a capture
         pos->rule50 = 0;
     }
 
@@ -247,9 +244,7 @@ bool MakeMove(Position *pos, const Move move) {
     // Pawn move specifics
     if (PieceTypeOf(pieceOn(to)) == PAWN) {
 
-        // Reset 50mr after a pawn move
         pos->rule50 = 0;
-
         Piece promo = promotion(move);
 
         // If the move is a pawnstart we set the en passant square and hash it in
@@ -274,7 +269,7 @@ bool MakeMove(Position *pos, const Move move) {
     HASH_SIDE;
 
     // If own king is attacked after the move, take it back immediately
-    if (SqAttacked(pos, Lsb(colorPieceBB(color, KING)), sideToMove)) {
+    if (SqAttacked(pos, Lsb(colorPieceBB(sideToMove^1, KING)), sideToMove)) {
         TakeMove(pos);
         return false;
     }
