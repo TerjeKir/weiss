3dbeae3d36691df48bb4ba89e5522d19b9f20e8b
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-09-08 17:24:30 +0200

    Bench 37068514

--- src/search.c
+++ src/search.c
@@ -282,6 +282,7 @@ static int AlphaBeta(Thread *thread, int alpha, int beta, Depth depth, PV *pv) {
     if (  !pvNode
         && history(-1).move != NOMOVE
         && eval >= beta
+        && history(0).eval >= beta
         && pos->nonPawnCount[sideToMove] > 0
         && depth >= 3
         && (!ttHit || !(tte->bound & BOUND_UPPER) || ttScore >= beta)) {
