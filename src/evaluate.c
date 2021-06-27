0457b94c2eb84f81bbb4d316cda51c903aa89ca1
TerjeKir <42723273+TerjeKir@users.noreply.github.com>
2021-06-27 20:29:30 +0200

    Tweak pawn support term (#464)
    Give bonus for number of pawns defending pawns rather than number of defended pawns.
    
    ELO   | 3.12 +- 3.00 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 3.04 (-2.94, 2.94) [-1.00, 4.00]
    Games | N: 22280 W: 4916 L: 4716 D: 12648

--- src/evaluate.c
+++ src/evaluate.c
@@ -123,8 +123,8 @@ INLINE int EvalPawns(const Position *pos, const Color color) {
     eval += PawnDoubled * count;
     TraceCount(PawnDoubled);
 
-    // Supported pawns
-    count = PopCount(pawns & PawnBBAttackBB(pawns, color));
+    // Pawns defending pawns
+    count = PopCount(pawns & PawnBBAttackBB(pawns, !color));
     eval += PawnSupport * count;
     TraceCount(PawnSupport);
 
