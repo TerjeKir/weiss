bed29860ba9d6403c0c022a28678f08261c77be1
Terje <42723273+TerjeKir@users.noreply.github.com>
2021-06-30 04:39:07 +0200

    .

--- src/tests.c
+++ src/tests.c
@@ -199,8 +199,8 @@ void Perft(char *str) {
 }
 
 void PrintEval(Position *pos) {
-    static PawnCache pc = { 0 };
-    printf("%d\n", sideToMove == WHITE ? EvalPosition(pos, pc) : -EvalPosition(pos, pc));
+    printf("%d\n", sideToMove == WHITE ?  EvalPosition(pos, threads->pawnCache)
+                                       : -EvalPosition(pos, threads->pawnCache));
     fflush(stdout);
 }
 #endif
