bf787fa17ed86592c5e63e2b7567968ba16fa237
Terje <42723273+TerjeKir@users.noreply.github.com>
2021-07-02 23:53:29 +0200

    version increment

--- src/uci.h
+++ src/uci.h
@@ -24,7 +24,7 @@
 #include "threads.h"
 
 
-#define NAME "Weiss 1.3-dev"
+#define NAME "Weiss 1.4"
 
 #define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 #define INPUT_SIZE 4096
