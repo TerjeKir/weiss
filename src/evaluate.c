b8a3eda9241a61f03b51a6bb5c4bc38e1e43001c
Terje <tkirstihagen@gmail.com>
2022-08-09 02:12:35 +0200

    Bench: 23058594

--- src/evaluate.c
+++ src/evaluate.c
@@ -331,9 +331,15 @@ INLINE int EvalPassedPawns(const Position *pos, const EvalInfo *ei, const Color 
         Square sq = PopLsb(&passers);
         Square forward = sq + up;
         int rank = RelativeRank(color, RankOf(sq));
+        int file = FileOf(sq);
         int r = rank - RANK_4;
 
         if (rank < RANK_4) continue;
+
+        Square promoSq = RelativeSquare(color, MakeSquare(RANK_8, file));
+        if (Distance(sq, promoSq) < Distance(kingSq(!color), promoSq) - ((!color) == sideToMove)) {
+            eval += S(0, 150);
+        }
 
         count = Distance(forward, kingSq( color));
         eval += count * PassedDistUs[r];
