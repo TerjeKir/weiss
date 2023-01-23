3550357dd68f798185d8ba9631b05c8ffec47bb9
Terje <tkirstihagen@gmail.com>
2023-01-23 14:44:27 +0100

    .

--- src/uci.h
+++ src/uci.h
@@ -26,7 +26,7 @@
 #define NAME "Weiss 2.1-dev"
 
 #define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-#define INPUT_SIZE 4096
+#define INPUT_SIZE 8192
 
 
 enum InputCommands {
