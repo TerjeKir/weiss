4cdc82a27ef05ec999a4290bcdafff15419c6aaf
Terje <tkirstihagen@gmail.com>
2024-11-23 16:27:45 +0100

    Bench: 22926137

--- src/search.c
+++ src/search.c
@@ -47,8 +47,8 @@ static int Reductions[2][32][32];
 CONSTR(1) InitReductions() {
     for (int depth = 1; depth < 32; ++depth)
         for (int moves = 1; moves < 32; ++moves)
-            Reductions[0][depth][moves] = 0.38 + log(depth) * log(moves) / 2.93, // capture
-            Reductions[1][depth][moves] = 1.82 + log(depth) * log(moves) / 2.68; // quiet
+            Reductions[0][depth][moves] = 0.43 + log(depth) * log(moves) / 3.31, // capture
+            Reductions[1][depth][moves] = 1.86 + log(depth) * log(moves) / 2.70; // quiet
 }
 
 // Checks whether a move was already searched in multi-pv mode
@@ -62,8 +62,8 @@ static bool AlreadySearchedMultiPV(Thread *thread, Move move) {
 // Correct the evaluation based on historic differences between eval and final score
 static int CorrectEval(Thread *thread, int eval, int rule50) {
     int correctedEval = eval + GetCorrectionHistory(thread);
-    if (rule50 > 12)
-        correctedEval *= (256 - rule50) / 256.0;
+    if (rule50 > 9)
+        correctedEval *= (265 - rule50) / 265.0;
     return CLAMP(correctedEval, -TBWIN_IN_MAX + 1, TBWIN_IN_MAX - 1);
 }
 
@@ -133,7 +133,7 @@ static int Quiescence(Thread *thread, Stack *ss, int alpha, const int beta) {
     if (eval > alpha)
         alpha = eval;
 
-    futility = eval + 75;
+    futility = eval + 82;
     bestScore = eval;
 
 moveloop:
@@ -334,18 +334,18 @@ static int AlphaBeta(Thread *thread, Stack *ss, int alpha, int beta, Depth depth
     // Reverse Futility Pruning
     if (   depth < 7
         && eval >= beta
-        && eval - 74 * (depth - improving) - (ss-1)->histScore / 120 >= beta
-        && (!ttMove || GetHistory(thread, ss, ttMove) > 7600))
+        && eval - 75 * (depth - improving) - (ss-1)->histScore / 111 >= beta
+        && (!ttMove || GetHistory(thread, ss, ttMove) > 7850))
         return eval;
 
     // Null Move Pruning
     if (   eval >= beta
         && eval >= ss->staticEval
-        && ss->staticEval >= beta + 168 - 23 * depth
-        && (ss-1)->histScore < 26500
+        && ss->staticEval >= beta + 152 - 22 * depth
+        && (ss-1)->histScore < 24750
         && pos->nonPawnCount[sideToMove] > (depth > 8)) {
 
-        Depth reduction = 3 + depth / 4 + MIN(3, (eval - beta) / 256);
+        Depth reduction = 3 + depth / 4 + MIN(3, (eval - beta) / 249);
 
         ss->continuation = &thread->continuation[0][0][EMPTY][0];
 
@@ -420,7 +420,7 @@ move_loop:
             && thread->doPruning
             && bestScore > -TBWIN_IN_MAX) {
 
-            int R = Reductions[quiet][MIN(31, depth)][MIN(31, moveCount)] - ss->histScore / 10000;
+            int R = Reductions[quiet][MIN(31, depth)][MIN(31, moveCount)] - ss->histScore / 9500;
             Depth lmrDepth = depth - 1 - R;
 
             // Quiet late move pruning
@@ -432,7 +432,7 @@ move_loop:
                 continue;
 
             // SEE pruning
-            if (lmrDepth < 7 && !SEE(pos, move, quiet ? -48 * depth : -66 * depth))
+            if (lmrDepth < 7 && !SEE(pos, move, quiet ? -48 * depth : -61 * depth))
                 continue;
         }
 
@@ -468,7 +468,7 @@ move_loop:
             // Singular - extend by 1 or 2 ply
             if (score < singularBeta) {
                 extension = 1;
-                if (!pvNode && score < singularBeta - 5 && ss->doubleExtensions <= 5)
+                if (!pvNode && score < singularBeta - 3 && ss->doubleExtensions <= 5)
                     extension = 2;
             // MultiCut - ttMove as well as at least one other move seem good enough to beat beta
             } else if (singularBeta >= beta)
@@ -500,7 +500,7 @@ skip_extensions:
             // Base reduction
             int r = Reductions[quiet][MIN(31, depth)][MIN(31, moveCount)];
             // Adjust reduction by move history
-            r -= ss->histScore / 9500;
+            r -= ss->histScore / 10250;
             // Reduce less in pv nodes
             r -= pvNode;
             // Reduce less when improving
@@ -519,7 +519,7 @@ skip_extensions:
 
             // Re-search with the same window at full depth if the reduced search failed high
             if (score > alpha && lmrDepth < newDepth) {
-                bool deeper = score > bestScore + 22 + 7 * (newDepth - lmrDepth);
+                bool deeper = score > bestScore + 20 + 7 * (newDepth - lmrDepth);
 
                 newDepth += deeper;
 
