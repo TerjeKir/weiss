62f9f272d5ad4c835efe08c6c033b195c81aa594
Terje <tkirstihagen@gmail.com>
2026-08-13 00:37:05 +0200

    Bench: 10625614

--- src/bitboard.h
+++ src/bitboard.h
@@ -143,7 +143,7 @@ INLINE Bitboard BB(const Square sq) {
 
 INLINE Bitboard rankBB(const int r) {
     assert(r >= RANK_1 && r <= RANK_8);
-    return rank1BB << (FILE_NB * r);
+    return ((uint64_t)rank1BB) << (FILE_NB * r);
 }
 
 INLINE Bitboard fileBB(const int f) {
